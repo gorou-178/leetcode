eaf96d394a43674d55686aeb511686da13e74daf
gurimmer <s.reianai@gmail.com>
2020-07-11 14:00:19 +0900

    add test

---
+++ src/app/Problems/LongestPalindromic/LongestPalindromic.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Problems\LongestPalindromic;
+
+class LongestPalindromic {
+
+    /**
+     * 5. Longest Palindromic Substring
+     * Given a string s, find the longest palindromic substring in s.
+     * You may assume that the maximum length of s is 1000.
+     *
+     * @param string $s
+     * @return string
+     */
+    public function longestPalindrome($s)
+    {
+        $length = strlen($s);
+
+        /**
+         * 回文
+         * 奇数の場合 abcba
+         * 偶数の場合 abba
+         */
+
+        return '';
+    }
+}
